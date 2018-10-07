4b61cc2acd8eb6116a53e137e9c3203df042f478
Amber <amber_ak@live.com>
2018-10-07 20:34:39 +0300

    Annotation on Map

--- Annotation on Map/ViewController.swift
+++ Annotation on Map/ViewController.swift
@@ -7,13 +7,34 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let span: MKCoordinateSpan = MKCoordinateSpanMake(0.1, 0.1)
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(33.731240, 73.037248)
+        let region: MKCoordinateRegion =  MKCoordinateRegionMake(location, span)
+        map.setRegion(region, animated: true)
+    
+        
+        let annotation = MKPointAnnotation()
+        
+        annotation.coordinate = location
+        annotation.title = "Faisal Mosque"
+        annotation.subtitle = "World's famous mosque"
+        map .addAnnotation(annotation)
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
